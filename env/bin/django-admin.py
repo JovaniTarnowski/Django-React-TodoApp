d5a5929b4c2818babebcf8a052e39c65d0ccaee1
jovanitarnowski@gmail.com <jovanitarnowski@gmail.com>
2020-03-29 17:52:44 -0300

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jovani/Downloads/todo_drf/env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ frontend/build/precache-manifest.ac5c6addb0dc49203bb764b6e25687ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b6a857b984e858bb20c89e39fc6bff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df4055ee6814d16e60a",
+    "url": "/static/css/main.37e759d6.chunk.css"
+  },
+  {
+    "revision": "e0f0dc00ae6ae6f602c3",
+    "url": "/static/js/2.e5f8debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5f8debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df4055ee6814d16e60a",
+    "url": "/static/js/main.4f7ae01f.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
